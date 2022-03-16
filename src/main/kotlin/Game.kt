b54c35d65faa4b7afd9c7f1b26e863d3b6c5d73a
mariusz <magik313@gmail.com>
2022-03-16 15:07:06 +0100

    w: rozbudowa aury

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,12 +1,22 @@
 fun main(args: Array<String>) {
     val name = "Mordowicz"
-    var healthPoints = 89
+    var healthPoints = 55
     val isBlessed = true
     val isImmortal = false
 
+    val karma = (Math.pow(Math.random(), (110 - healthPoints)/ 100.0) * 20).toInt()
+
 //    Aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "ZIELONKAWA" else  "BRAK"
+    val auraColor = if (auraVisible) {
+        when (karma){
+            in 0..5 -> "czerwony"
+            in 6..10 -> "pomaranczowy"
+            in 11..15 -> "purpurowy"
+            in 16..20 -> "zielonkawy"
+            else -> "brak"
+        }
+    } else  "BRAK"
 
     val healthStatus = when (healthPoints){
         100 -> "cieszy sie swietnym zdrowiem!"
@@ -19,7 +29,7 @@ fun main(args: Array<String>) {
         in 15..75 -> "jest ciezko ranny"
         else -> "jest w oplakanym stanie"
     }
-    
+
     println("(Aura: $auraColor) " +
             "(Pobłogosławiony: ${if (isBlessed) "TAK" else "NIE"})")
     println("$name $healthStatus")
