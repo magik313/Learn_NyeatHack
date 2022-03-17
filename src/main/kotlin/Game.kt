830824d0526e9052fb73ea6323e6390e1c664c46
mariusz <magik313@gmail.com>
2022-03-17 15:37:45 +0100

    rozdzial 4

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -5,22 +5,48 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
 //    Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "ZIELONKAWA" else  "BRAK"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
-    val healthStatus = when (healthPoints){
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Pobłogosławiony: ${if (isBlessed) "TAK" else "NIE"})"
+    )
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "ZIELONKAWA" else "BRAK"
+    return auraColor
+}
+
+
+    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean)
+    = when (healthPoints) {
         100 -> "cieszy sie swietnym zdrowiem!"
         in 90..99 -> "jest zadrapany"
-        in 75..89 -> if (isBlessed){
+        in 75..89 -> if (isBlessed) {
             " jest lekko poobijany ale sie leczy"
-        }else {
+        } else {
             " jest lekko poobijany"
         }
         in 15..75 -> "jest ciezko ranny"
         else -> "jest w oplakanym stanie"
     }
-    
-    println("(Aura: $auraColor) " +
-            "(Pobłogosławiony: ${if (isBlessed) "TAK" else "NIE"})")
-    println("$name $healthStatus")
-}
+
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("nagle pojawila sie kula ognia (x$numFireballs)")
