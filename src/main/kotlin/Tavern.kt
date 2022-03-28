67392651721e8a246156d99549f64a12d07a321c
mariusz <magik313@gmail.com>
2022-03-28 21:31:39 +0200

    rozdzial7

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,15 +1,38 @@
+
+const val TAVERN_NAME = "Szynk Hipolit'a"
+
 fun main(args: Array<String>) {
-    var beverage = readLine()
+//   placeOrder("shandy,Oddech Smoka,5.91")
+    placeOrder("eliksir,Zdrowy wykop,4.83")
 
-    beverage = null
+}
 
-    if (beverage != null){
-        beverage = beverage.capitalize()
-    }else{
-        println("beverage rowne null")
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
     }
 
-    val beverageServed: String = beverage ?: "Grzaniec na Porterze"
-    print(beverageServed)
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(6 until indexOfApostrophe)
+    println("Mordowicz i $tavernMaster rozmawiaja o zamowieniu")
 
+    val (type, name, price) = menuData.split(',')
+    val message = "Mordowycz kupil $name ($type) za $price."
+    println(message)
+
+
+    val phrase = if (name == "Oddech Smoka"){
+        "Mordowicz konstatuuje z zachwytem: ${toDragonSpeak("No... Ależ doskonały jest ten $name")}"
+    } else {
+        "Mordowicz mowi: dziekuję za ${name}"
+    }
+    println(phrase)
 }
